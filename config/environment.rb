3cb6f723a9e895a3202f523dca3ea3bbedb6921e
Thomaz de Oliveira dos Reis <thomaz.reis@ebc.com.br>
2013-05-07 11:20:46 -0300

    First Commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Raia8::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raia8::Application.config.secret_token = '2e3966492f50a2af5065a3bb2040a3291ba978515054294b7201315061cda0d11b6e65842575e3572f49afb55e36e4c5e58f296443f559a9ad0b43a795920c6a'
