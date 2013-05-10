4a4b50acdf94f67f126f2a762af1ede0f121208c
Thomaz de Oliveira dos Reis <thor27@gmail.com>
2013-05-10 10:32:16 -0300

    Postgres! :suspect:

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Raia8
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # This is because of heroku 
+    config.assets.initialize_on_precompile = false
   end
 end
